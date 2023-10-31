d983fa8c35eb1cd7137dac03862d22bfb8d3d3c0
josylmaro <josyl.m.b.quiambao@accenture.com>
2023-10-31 19:04:49 +0800

    update heading

--- sample-app/src/components/booksmanager/common/bookCard.tsx
+++ sample-app/src/components/booksmanager/common/bookCard.tsx
@@ -5,7 +5,7 @@ import {Card, CardHeader, CardBody, CardText } from 'reactstrap'
 function BookCard ({title, books} : {title: string, books: Array<Book>}) {
     return ( 
         <Card className='mt-4'>
-            <CardHeader className='blue-header text-center'><h1>{title}</h1></CardHeader>
+            <CardHeader className='blue-header text-center'><h2>{title}</h2></CardHeader>
             <CardBody>
                 { books?.map((item,key) => {
                     return (
